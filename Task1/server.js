3a3d4875ec1dbd7faa3440d86c3f57c1bdecad58
maheshwararaj07 <maheshwararaj2003@gmail.com>
2024-05-18 09:17:02 +0530

    API connected

--- Task1/server.js
+++ Task1/server.js
@@ -8,28 +8,24 @@ const PORT = process.env.PORT || 3000;
 // Set up body parser middleware
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Set EJS as view engine
+// // Set EJS as view engine
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 // Serve static files from the public directory
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public','index.html')));
 
 // Route to serve the index page
 app.get('/', (req, res) => {
-    // Here you can dynamically generate data to pass to the template
-    const username = "John"; // Example dynamic data
-    
-    // Render the index.ejs template with dynamic data
-    res.render('index', { username: username });
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 // Route to handle form submission
 app.post('/submit', (req, res) => {
     const { name, email, message } = req.body;
     // Here you can handle the form submission, for example, saving data to a database
-    console.log(`Received form submission:\nName: ${name}\nEmail: ${email}\nMessage: ${message}`);
-    res.render("success",{name:name})
+    
+    res.render("success",{name:name,email:email,message:message})
 });
 
 // Start server
